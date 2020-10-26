4c2259b3e6b9a90bbbc8d9eff3fea2a17154e8d7
Sylvain Legault <slegault@aldogroup.com>
2020-10-26 10:01:49 -0400

    remove aws dependency, simple fetch

--- src/plugin.js
+++ src/plugin.js
@@ -1,93 +1,120 @@
-const AWSCognito = require('amazon-cognito-identity-js')
-const jwtDecode = require('jwt-decode');
-const CryptoJS = require('crypto-js')
+const AWSCognito = require("amazon-cognito-identity-js")
+const jwtDecode = require("jwt-decode")
+const CryptoJS = require("crypto-js")
 
 // Get JWT Token from Cognito
-const session = ({ Username, Password, UserPoolId, ClientId, TokenType }) => new Promise((resolve, reject) => {
-  new AWSCognito.CognitoUser({
-    Username,
-    Pool: new AWSCognito.CognitoUserPool({
-      UserPoolId,
-      ClientId,
-    })
-  }).authenticateUser(new AWSCognito.AuthenticationDetails({
-    Username,
-    Password
-  }),
-  {
-    onSuccess: result => {
-      TokenType === 'id' ? resolve(result.idToken.jwtToken): resolve(result.accessToken.jwtToken);
+const session = async ({
+  Username,
+  Password,
+  Region,
+  ClientId,
+  TokenType,
+}) => {
+  const domain = Region.split('_')[0] // backward compatible with Pool
+  const response = await fetch(`https://cognito-idp.${domain}.amazonaws.com`, {
+    method: "post",
+    headers: {
+      "content-type": "application/x-amz-json-1.1",
+      "x-amz-target": "AWSCognitoIdentityProviderService.InitiateAuth",
     },
-    onFailure: error => {
-      console.log(error);
-      reject(error);
-    }
-  });
-});
+    body: JSON.stringify({
+      AuthFlow: "USER_PASSWORD_AUTH",
+      ClientId,
+      AuthParameters: {
+        USERNAME: Username,
+        PASSWORD: Password,
+      },
+    }),
+  })
+  if (response.status !== 200) {
+    console.log(
+      "Looks like there was a problem. Status Code: " + response.status
+    )
+    return
+  }
+  const { AuthenticationResult } = await response.json()
+  return AuthenticationResult
+    ? TokenType === "id"
+      ? AuthenticationResult.IdToken
+      : AuthenticationResult.AccessToken
+    : undefined
+}
+
 
 // Validate if the token has expired
-const validToken = token => {
+const validToken = (token) => {
   const now = Date.now().valueOf() / 1000
-  const data = jwtDecode(token)
-  if (typeof data.exp !== 'undefined' && data.exp < now) {
+  try {
+    const data = jwtDecode(token)
+    if (typeof data.exp !== "undefined" && data.exp < now) {
+      return false
+    }
+    if (typeof data.nbf !== "undefined" && data.nbf > now) {
+      return false
+    }
+    return true
+  } catch (err) {
     return false
   }
-  if (typeof data.nbf !== 'undefined' && data.nbf > now) {
-    return false
-  }
-  return true
 }
 
 // Encode our token
 const base64url = (source) => {
-  encodedSource = CryptoJS.enc.Base64.stringify(source);
-  encodedSource = encodedSource.replace(/=+$/, '');
-  encodedSource = encodedSource.replace(/\+/g, '-');
-  encodedSource = encodedSource.replace(/\//g, '_');
-  return encodedSource;
+  encodedSource = CryptoJS.enc.Base64.stringify(source)
+  encodedSource = encodedSource.replace(/=+$/, "")
+  encodedSource = encodedSource.replace(/\+/g, "-")
+  encodedSource = encodedSource.replace(/\//g, "_")
+  return encodedSource
 }
 
 // Create a fake token to keep in store, so we don't query for same wrong values
-const errorToken = error => {
+const errorToken = (error) => {
   const header = {
-    "alg": "HS256",
-    "typ": "JWT"
-  };
-  const stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
-  const encodedHeader = base64url(stringifiedHeader);
+    alg: "HS256",
+    typ: "JWT",
+  }
+  const stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header))
+  const encodedHeader = base64url(stringifiedHeader)
   // If error we keep it for 1 min
-  const exp = (Date.now().valueOf() / 1000) + 60
+  const exp = Date.now().valueOf() / 1000 + 60
   const data = {
     error,
-    exp
-  };
-  const stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
-  const encodedData = base64url(stringifiedData);
-  return encodedHeader + "." + encodedData;
+    exp,
+  }
+  const stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data))
+  const encodedData = base64url(stringifiedData)
+  return encodedHeader + "." + encodedData
 }
 
 // Main run function
-const run = async (context, Username, Password, UserPoolId, ClientId, TokenType) => {
+const run = async (
+  context,
+  Username,
+  Password,
+  Region,
+  ClientId,
+  TokenType
+) => {
   if (!Username) {
-    throw new Error('Username attribute is required')
+    throw new Error("Username attribute is required")
   }
   if (!Password) {
-    throw new Error('Password attribute is required')
+    throw new Error("Password attribute is required")
   }
-  if (!UserPoolId) {
-    throw new Error('UserPoolId attribute is required')
+  if (!Region) {
+    throw new Error("Region attribute is required")
   }
   if (!ClientId) {
-    throw new Error('ClientId attribute is required')
+    throw new Error("ClientId attribute is required")
   }
   if (!TokenType) {
-    TokenType = 'access'
+    TokenType = "access"
   }
 
-  const key = [ Username, Password, UserPoolId, ClientId, TokenType ].join('::')
+  const key = [Username, Password, Region, ClientId, TokenType].join("::")
   const token = await context.store.getItem(key)
   if (token && validToken(token)) {
-    if (jwtDecode(token).error){
+    if (jwtDecode(token).error) {
       // Display error
       return jwtDecode(token).error
     }
@@ -96,11 +123,16 @@ const run = async (context, Username, Password, UserPoolId, ClientId, TokenType)
   } else {
     // Compute a new token
     try {
-      const token = await session({ Username, Password, UserPoolId, ClientId, TokenType })
+      const token = await session({
+        Username,
+        Password,
+        Region,
+        ClientId,
+        TokenType,
+      })
       await context.store.setItem(key, token)
       return token
-    }
-    catch(error){
+    } catch (error) {
       // To keep thing simle we create a fake JWT token with error message
       const token = errorToken(error.message)
       await context.store.setItem(key, token)
@@ -109,46 +141,48 @@ const run = async (context, Username, Password, UserPoolId, ClientId, TokenType)
   }
 }
 
-module.exports.templateTags = [{
-  name: 'AwsCognitoToken',
-  displayName: 'AWS Cognito Token',
-  description: 'Plugin for Insomnia to provide Cognito JWT token from AWS',
-  args: [
-    {
-      displayName: 'Username',
-      type: 'string',
-      validate: arg => (arg ? '' : 'Required')
-    },
-    {
-      displayName: 'Password',
-      type: 'string',
-      validate: arg => (arg ? '' : 'Required')
-    },
-    {
-      displayName: 'UserPoolId',
-      type: 'string',
-      validate: arg => (arg ? '' : 'Required')
-    },
-    {
-      displayName: 'ClientId',
-      type: 'string',
-      validate: arg => (arg ? '' : 'Required')
-    },
-    {
-      displayName: "TokenType",
-      type: "enum",
-      defaultValue: "access",
-      options: [
-        {
-          displayName: "access",
-          value: "access"
-        },
-        {
-          displayName: "id",
-          value: "id"
-        }
-      ]
-    },
-  ],
-  run
-}];
+module.exports.templateTags = [
+  {
+    name: "AwsCognitoToken",
+    displayName: "AWS Cognito Token",
+    description: "Plugin for Insomnia to provide Cognito JWT token from AWS",
+    args: [
+      {
+        displayName: "Username",
+        type: "string",
+        validate: (arg) => (arg ? "" : "Required"),
+      },
+      {
+        displayName: "Password",
+        type: "string",
+        validate: (arg) => (arg ? "" : "Required"),
+      },
+      {
+        displayName: "Region",
+        type: "string",
+        validate: (arg) => (arg ? "" : "Required"),
+      },
+      {
+        displayName: "ClientId",
+        type: "string",
+        validate: (arg) => (arg ? "" : "Required"),
+      },
+      {
+        displayName: "TokenType",
+        type: "enum",
+        defaultValue: "access",
+        options: [
+          {
+            displayName: "access",
+            value: "access",
+          },
+          {
+            displayName: "id",
+            value: "id",
+          },
+        ],
+      },
+    ],
+    run,
+  },
+]
